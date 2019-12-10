8f8464244ed1e138669417cb1d67a972b2f5ae74
SURFACEBOOK2\jackwynne <jack.wynne@outlook.com>
2019-12-11 10:30:00 +1300

    CAPM

--- PortfolioPerformanceUnconditionalPattern.m
+++ PortfolioPerformanceUnconditionalPattern.m
@@ -21,4 +21,19 @@ corrs = corr(zdata);
 sharpe = (lsdata - rf10) ./ std_dev;
 
 %% 1.2 Risk-Adjusted Returns
-regress(
+% CAPM
+x1 = ones(length(lsdata),1);
+capm_reg = table;
+for i = 1:11
+    X1 = ff3.mkt;
+    X = [x1 X1];
+    t = table;
+    t.i = i;
+    [b,bint,r,rint,stats] = regress(lsdata(:,i), X);
+    t.a = b(1);    t.bMkt = b(2);
+    t.R2 = stats(1);    t.fStat = stats(2);
+    t.pv = stats(3);    t.errorVar = stats(4);
+    capm_reg = [capm_reg; t];
+end
+
+%FF3
