98ab0746ae82090bcf13a69dc5a4ec40080f726c
SURFACEBOOK2\jackwynne <jack.wynne@outlook.com>
2019-12-10 16:02:28 +1300

    added return calc for gpa and ag

--- decile.m
+++ decile.m
@@ -1,13 +1,14 @@
-clear; clc; load('crsp+an.mat');
+clear; clc; load('anomalies2.mat');
 
 minyear = min(crspa.fyear);     maxyear = max(crspa.fyear);
-year = crspa.fyear;
+year = crspa.fyear;             nyears = maxyear - minyear;
 
 gpa = crspa.gpa;        ag = crspa.ag;      iva = crspa.iva;
 noa = crspa.noa;        ac = crspa.ac;      nsi = crspa.nsi;
 cei = crspa.cei;
 
 %% Calculate Deciles
+h = waitbar(0,'Please wait for deciles...');
 for y = minyear:maxyear
     fts_y = (year == y); 
     gpa_y = gpa(fts_y, 1);
@@ -33,18 +34,31 @@ for y = minyear:maxyear
     crspa.ac_dec(fts_y, 1) = ac_dec;
     crspa.nsi_dec(fts_y, 1) = nsi_dec;
     crspa.cei_dec(fts_y, 1) = cei_dec;
+    waitbar(y / nyears)
 end
+close(h)
 
 %% Calculate Equal Weighted Returns
-gpa_dec = crspa.gpa_dec;
-
+gpa_dec = crspa.gpa_dec;    ag_dec = crspa.ag_dec;
+anomalies = table();
+h = waitbar(0,'Please wait for anomalies...');
 for d = 1:10
     gpa_d = (gpa_dec == d);
+    ag_d = (ag_dec == d);
     for y = minyear:maxyear
+        a = table();
         fts_y = (year == y); 
         gpa_d_y = gpa(gpa_d == 1 & fts_y == 1);
+        ag_d_y = ag(ag_d == 1 & fts_y == 1);
         gpa_ret = mean(gpa_d_y);
-        anomalies.year = y
-        anomalies.gpa_ret(
+        ag_ret = mean(ag_d_y);
+        a.year = y;
+        a.dec = d;
+        a.gpa_ret = gpa_ret;
+        a.ag_ret = ag_ret;
+        anomalies = [anomalies;a];
+        waitbar((y / nyears + d / 10)/2)
     end
 end
+close(h)
+save("decile.mat")
