6c27d05813157bc3a2fc3f56855cea42c0429ba3
SURFACEBOOK2\jackwynne <jack.wynne@outlook.com>
2019-12-10 16:58:19 +1300

    long minus short calc

---
+++ regression.m
@@ -0,0 +1,12 @@
+clear; clc; load('decile.mat');
+
+%% Long minus Short
+ls = sortrows(anomalies, 'year');
+long = ls(ls.dec == 10, :);
+short = ls(ls.dec == 1, :);
+
+lms = table;
+lms.year = long.year;
+lms.gpa_lms = long.gpa_ret - short.gpa_ret;
+lms.ag_lms = long.ag_ret - short.ag_ret;
+
