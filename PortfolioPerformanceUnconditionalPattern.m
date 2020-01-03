894fe47e983b8c5b8a3461136e02ae8545e76593
SURFACEBOOK2\jackwynne <jack.wynne@outlook.com>
2020-01-04 12:31:55 +1300

    changed to format of cutting to length of vix data

--- PortfolioPerformanceUnconditionalPattern.m
+++ PortfolioPerformanceUnconditionalPattern.m
@@ -27,63 +27,70 @@ corrs = corr(zdata);
 sharpe = (lsdata - rf10) ./ std_dev;
 
 %% 1.2 Risk-Adjusted Returns
+filter = ones(height(vix),1);
+AnoLSS_filter = (AnoLSS.YYYYMM >= min(vix.Date)) + (AnoLSS.YYYYMM == datetime(1986,1,1));
+AnoLSSy = AnoLSS(AnoLSS_filter == 1, :);
+Anof = AnoLSSy(filter == 1, :);
+
+ff3_filter = (ff3.year >= min(vix.Date)) + (ff3.year == datetime(1986,1,1));
+ff3y = ff3(ff3_filter == 1, :);
+ff3f = ff3y(filter == 1, :);
 % CAPM
 for i = 1:11
-    X1 = ff3.mkt_rf;
+    X1 = ff3f.mkt_rf;
     X = [X1];
-    col = (i*3);
-    Y = lsdata(:, col);
+    col = (i*3)+1;
+    Y = table2array(Anof(:,col));
     mdl = fitlm(X, Y);
     capm_reg{i} = mdl;
 end
 
 %FF3
 for i = 1:11
-    X1 = ff3.mkt_rf;    X2 = ff3.smb;    X3 = ff3.hml;
+    X1 = ff3f.mkt_rf;    X2 = ff3f.smb;    X3 = ff3f.hml;
     X = [X1 X2 X3];
-    col = (i*3);
-    Y = lsdata(:, col);
+    col = (i*3)+1;
+    Y = table2array(Anof(:,col));
     mdl = fitlm(X, Y);
     ff3_reg{i} = mdl;
 end
 
+ff5_filter = (ff5.year >= min(vix.Date)) + (ff5.year == datetime(1986,1,1));
+ff5y = ff5(ff5_filter == 1, :);
+ff5f = ff5y(filter == 1, :);
 % FF5
 for i = 1:11
-    X1 = ff5.mkt_rf;    X2 = ff5.smb;    X3 = ff5.hml;
-    X4 = ff5.rmw;       X5 = ff5.cma;
+    X1 = ff5f.mkt_rf;    X2 = ff5f.smb;    X3 = ff5f.hml;
+    X4 = ff5f.rmw;       X5 = ff5f.cma;
     X = [X1 X2 X3 X4 X5];
-    col = (i*3);
-    Y = lsdata(7:end, col);
+    col = (i*3)+1;
+    Y = table2array(Anof(:,col));
     mdl = fitlm(X, Y);
     ff5_reg{i} = mdl; 
 end
 
+fffw_filter = (fffw.year >= min(vix.Date)) + (fffw.year == datetime(1986,1,1));
+fffwy = fffw(fffw_filter == 1, :);
+fffwf = fffwy(filter == 1, :);
 % FF6 (FF5 + Momentum)
 for i = 1:11
-    X1 = ff5.mkt_rf;    X2 = ff5.smb;    X3 = ff5.hml;
-    X4 = ff5.rmw;       X5 = ff5.cma;    X6 = fffw.umd(7:end,1);
+    X1 = ff5f.mkt_rf;    X2 = ff5f.smb;    X3 = ff5f.hml;
+    X4 = ff5f.rmw;       X5 = ff5f.cma;    X6 = fffwf.umd;
     X = [X1 X2 X3 X4 X5 X6];
-    col = (i*3);
-    Y = lsdata(7:end,col);
+    col = (i*3)+1;
+    Y = table2array(Anof(:,col));
     mdl = fitlm(X, Y);
     ff6_reg{i} = mdl; 
 end
 
 % FF6 + VIX
-ff5_filter = (ff5.year >= min(vix.Date)) + (ff5.year == datetime(1986,1,1));
-fffw_filter = (fffw.year >= min(vix.Date)) + (fffw.year == datetime(1986,1,1));
-AnoLSS_filter = (AnoLSS.YYYYMM >= min(vix.Date)) + (AnoLSS.YYYYMM == datetime(1986,1,1));
-ff5f = ff5(ff5_filter == 1, :);
-fffwf = fffw(fffw_filter == 1, :);
-AnoLSSf = AnoLSS(AnoLSS_filter == 1, :);
-x1 = ones(height(vix),1);
 for i = 1:11 
     X1 = ff5f.mkt_rf;    X2 = ff5f.smb;    X3 = ff5f.hml;
     X4 = ff5f.rmw;       X5 = ff5f.cma;    X6 = fffwf.umd;
-    X7 = vix.vxo;
+    X7 = vix.vxo(filter == 1, :);
     X = [X1 X2 X3 X4 X5 X6 X7];
     col = (i*3)+1;
-    Y = table2array(AnoLSSf(:,col));
+    Y = table2array(Anof(:,col));
     mdl = fitlm(X, Y);
     ff6vix_reg{i} = mdl;   
 end
