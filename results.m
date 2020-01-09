9f951224d6d02ba0c995b474c762caa0f016d519
SURFACEBOOK2\jackwynne <jack.wynne@outlook.com>
2020-01-09 14:37:27 +1300

    All stats from nice functions

--- results.m
+++ results.m
@@ -25,99 +25,27 @@ unconstats = transpose(unconstats);
 [res_ff6, rese_ff6, rest_ff6] = unconres(ff6_reg);
 [res_ff6vix, rese_ff6vix, rest_ff6vix] = unconres(ff6vix_reg);
 
+[res_vol_stats, res_vol_stats_mean, res_vol_stats_stdev] = constats(highvol_stats, lowvol_stats);
+[res_epu_stats, res_epu_stats_mean, res_epu_stats_stdev] = constats(highepu_stats, lowepu_stats);
+[res_liq_stats, res_liq_stats_mean, res_liq_stats_stdev] = constats(highliq_stats,lowliq_stats);
 
-res_vol_stats = [];
-res_vol_stats_mean = [];
-res_vol_stats_stdev = [];
-for x = 1:11
-    means = [];
-    means = [means, highvol_stats{2}(x)];
-    means = [means, lowvol_stats{2}(x)];
-    means = [means, highvol_stats{3}(x)];
-    means = [means, lowvol_stats{3}(x)];
-    means = [means, highvol_stats{1}(x)];
-    means = [means, lowvol_stats{1}(x)];
-    
-    stdev = [];
-    stdev = [stdev, highvol_stats{5}(x)];
-    stdev = [stdev, lowvol_stats{5}(x)];
-    stdev = [stdev, highvol_stats{6}(x)];
-    stdev = [stdev, lowvol_stats{6}(x)];
-    stdev = [stdev, highvol_stats{4}(x)];
-    stdev = [stdev, lowvol_stats{4}(x)];
-    
-    res_vol_stats = vertcat(res_vol_stats, means);
-    res_vol_stats = vertcat(res_vol_stats, stdev);
+[res_vol_capm, rese_vol_capm, rest_vol_capm] = conres(highvol_capm_reg, lowvol_capm_reg);
+[res_vol_ff3, rese_vol_ff3, rest_vol_ff3] = conres(highvol_ff3_reg, lowvol_ff3_reg);
+[res_vol_ff5, rese_vol_ff5, rest_vol_ff5] = conres(highvol_ff5_reg, lowvol_ff5_reg);
+[res_vol_ff6, rese_vol_ff6, rest_vol_ff6] = conres(highvol_ff6_reg, lowvol_ff6_reg);
+[res_vol_ff6vix, rese_vol_ff6vix, rest_vol_ff6vix] = conres(highvol_ff6vix_reg, lowvol_ff6vix_reg);
 
-    res_vol_stats_mean = vertcat(res_vol_stats_mean, means);
-    res_vol_stats_stdev = vertcat(res_vol_stats_stdev, stdev);
-end
+[res_epu_capm, rese_epu_capm, rest_epu_capm] = conres(highepu_capm_reg, lowepu_capm_reg);
+[res_epu_ff3, rese_epu_ff3, rest_epu_ff3] = conres(highepu_ff3_reg, lowepu_ff3_reg);
+[res_epu_ff5, rese_epu_ff5, rest_epu_ff5] = conres(highepu_ff5_reg, lowepu_ff5_reg);
+[res_epu_ff6, rese_epu_ff6, rest_epu_ff6] = conres(highepu_ff6_reg, lowepu_ff6_reg);
+[res_epu_ff6vix, rese_epu_ff6vix, rest_epu_ff6vix] = conres(highepu_ff6vix_reg, lowepu_ff6vix_reg);
 
-res_epu_stats = [];
-res_epu_stats_mean = [];
-res_epu_stats_stdev = [];
-for x = 1:11
-    means = [];
-    means = [means, highepu_stats{2}(x)];
-    means = [means, lowepu_stats{2}(x)];
-    means = [means, highepu_stats{3}(x)];
-    means = [means, lowepu_stats{3}(x)];
-    means = [means, highepu_stats{1}(x)];
-    means = [means, lowepu_stats{1}(x)];
-    
-    stdev = [];
-    stdev = [stdev, highepu_stats{5}(x)];
-    stdev = [stdev, lowepu_stats{5}(x)];
-    stdev = [stdev, highepu_stats{6}(x)];
-    stdev = [stdev, lowepu_stats{6}(x)];
-    stdev = [stdev, highepu_stats{4}(x)];
-    stdev = [stdev, lowepu_stats{4}(x)];
-    
-    res_epu_stats = vertcat(res_epu_stats, means);
-    res_epu_stats = vertcat(res_epu_stats, stdev);
-
-    res_epu_stats_mean = vertcat(res_epu_stats_mean, means);
-    res_epu_stats_stdev = vertcat(res_epu_stats_stdev, stdev);
-end
-
-res_liq_stats = [];
-res_liq_stats_mean = [];
-res_liq_stats_stdev = [];
-for x = 1:11
-    means = [];
-    means = [means, highliq_stats{2}(x)];
-    means = [means, lowliq_stats{2}(x)];
-    means = [means, highliq_stats{3}(x)];
-    means = [means, lowliq_stats{3}(x)];
-    means = [means, highliq_stats{1}(x)];
-    means = [means, lowliq_stats{1}(x)];
-    
-    stdev = [];
-    stdev = [stdev, highliq_stats{5}(x)];
-    stdev = [stdev, lowliq_stats{5}(x)];
-    stdev = [stdev, highliq_stats{6}(x)];
-    stdev = [stdev, lowliq_stats{6}(x)];
-    stdev = [stdev, highliq_stats{4}(x)];
-    stdev = [stdev, lowliq_stats{4}(x)];
-    
-    res_liq_stats = vertcat(res_liq_stats, means);
-    res_liq_stats = vertcat(res_liq_stats, stdev);
-
-    res_liq_stats_mean = vertcat(res_liq_stats_mean, means);
-    res_liq_stats_stdev = vertcat(res_liq_stats_stdev, stdev);
-end
-
-
-% res_highv_ff6vix = [];
-% rese_highv_ff6vix = [];
-% resp_highv_ff6vix = [];
-% for x = 1:11
-%     estimate = transpose(highvol_ff6vix_reg{x}.Coefficients.Estimate);
-%     pval = transpose(highvol_ff6vix_reg{x}.Coefficients.pValue);
-%     res_highv_ff6vix = vertcat(res_highv_ff6vix, estimate);
-%     rese_highv_ff6vix = vertcat(rese_highv_ff6vix, estimate);
-%     resp_highv_ff6vix = vertcat(resp_highv_ff6vix, pval);
-% end
+[res_liq_capm, rese_liq_capm, rest_liq_capm] = conres(highliq_capm_reg, lowliq_capm_reg);
+[res_liq_ff3, rese_liq_ff3, rest_liq_ff3] = conres(highliq_ff3_reg, lowliq_ff3_reg);
+[res_liq_ff5, rese_liq_ff5, rest_liq_ff5] = conres(highliq_ff5_reg, lowliq_ff5_reg);
+[res_liq_ff6, rese_liq_ff6, rest_liq_ff6] = conres(highliq_ff6_reg, lowliq_ff6_reg);
+[res_liq_ff6vix, rese_liq_ff6vix, rest_liq_ff6vix] = conres(highliq_ff6vix_reg, lowliq_ff6vix_reg);
     
 function [res, rese, rest] = unconres(mdl)
     res = [];
@@ -126,6 +54,49 @@ function [res, rese, rest] = unconres(mdl)
     for x = 1:11
         estimate = transpose(mdl{x}.Estimate);
         tstat = transpose(mdl{x}.tStat);
+        res = vertcat(res, estimate);
+        res = vertcat(res, tstat);
+        rese = vertcat(rese, estimate);
+        rest = vertcat(rest, tstat);
+    end
+end
+
+function [stats, stats_mean, stats_stdev] = constats(high_stats,low_stats)
+    stats = [];
+    stats_mean = [];
+    stats_stdev = [];
+    for x = 1:11
+        means = [];
+        means = [means, high_stats{2}(x)];
+        means = [means, high_stats{3}(x)];
+        means = [means, high_stats{1}(x)];
+        means = [means, low_stats{2}(x)];
+        means = [means, low_stats{3}(x)];
+        means = [means, low_stats{1}(x)];
+
+        stdev = [];
+        stdev = [stdev, high_stats{5}(x)];
+        stdev = [stdev, high_stats{6}(x)];
+        stdev = [stdev, high_stats{4}(x)];
+        stdev = [stdev, low_stats{5}(x)];
+        stdev = [stdev, low_stats{6}(x)];
+        stdev = [stdev, low_stats{4}(x)];
+
+        stats = vertcat(stats, means);
+        stats = vertcat(stats, stdev);
+
+        stats_mean = vertcat(stats_mean, means);
+        stats_stdev = vertcat(stats_stdev, stdev);
+    end
+end
+
+function [res, rese, rest] = conres(high_mdl, low_mdl)
+    res = [];
+    rese = [];
+    rest = [];
+    for x = 1:11
+        estimate = horzcat(transpose(high_mdl{x}.Estimate), transpose(low_mdl{x}.Estimate));
+        tstat = horzcat(transpose(high_mdl{x}.tStat), transpose(low_mdl{x}.tStat));
         res = vertcat(res, estimate);
         res = vertcat(res, tstat);
         rese = vertcat(rese, estimate);
